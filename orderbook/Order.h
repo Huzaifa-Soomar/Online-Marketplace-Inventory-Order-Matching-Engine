b40c453aa8e17030d1d879689b7c4e041324220a
Huzaifa-Soomar <huzaifasoomar78@gmail.com>
2025-12-17 21:38:06 +0500

    comparision for heap

--- orderbook/Order.h
+++ orderbook/Order.h
@@ -15,7 +15,15 @@ public:
     double price;
     long long timestamp;
 
-    Order(int id = 0, int pid = 0, OrderType t = BUY, int qty = 0, double pr = 0.0, long long ts = 0);
+    Order(int id = 0, int pid = 0, OrderType t = BUY,
+          int qty = 0, double pr = 0.0, long long ts = 0);
+
+    // Comparison for heap
+    bool operator<(const Order& other) const {
+        if (price == other.price)
+            return timestamp > other.timestamp; // earlier first
+        return price < other.price;
+    }
 };
 
 #endif
